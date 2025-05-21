a9bc77c9ffc8d104c9906c5894b8e082866cb706
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-05-20 19:29:47 -0700

    obj contructor

--- scripts/register.js
+++ scripts/register.js
@@ -1,35 +1,45 @@
-console.log("register");
+let pets=[];
 
-let pet1 = {
-    name:"Scooby",
-    age:60,
-    gender:"Male"
-}
-let pet2 = {
-    name:"Scrappy",
-    age:50,
-    gender:"Male"
-}
-let pet3={
-    name:"Tweety",
-    age:70,
-    gender:"Female"
+let inputName = document.getElementById("txtName");
+let inputAge = document.getElementById("txtAge");
+let inputGender = document.getElementById("txtGender");
+let inputBreed = document.getElementById("txtBreed");
+
+//creating the obj constructor (name, age, gender, breed)
+function Pet(name,age,gender,breed){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
 }
 
-let pets = []; 
+function register(){
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value);
+    pets.push(newPet);
+    console.log(newPet);
+    alert("The registration was successful!!!");
+    displayInfo();
+    clearForm();
+}
 
-pets.push(pet1,pet2,pet3);
-console.log(pets);
+function displayInfo(){
+    // display the total registered pets
+    document.getElementById("info").innerHTML=`<p>Total of pets: ${pets.length}</p>`
+}
 
-function displayNames(){
-    for(let i=0;i<pets.length;i++){
-        console.log(pets[0].name);
-        // display on the HTML
-    }
-   
-
-    console.log("We have " + pets.length + " pets");
+function clearForm(){
 
 }
 
-displayNames();
+function init(){
+    //create objs
+    let pet1 = new Pet("Scooby",99,"Male","Dane");
+    let pet2 = new Pet("Scrappy",80,"Male","Mixed");
+    let pet3 = new Pet("Tweety bird",99,"Male","Canarian");
+    pets.push(pet1,pet2,pet3);
+    console.log(pets);
+
+    displayInfo();
+}
+
+window.onload=init; //render the HTML
